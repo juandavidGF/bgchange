eb9f8e31522f14af0c5c788c8f1aa21f44170392
juandavidGF <davad701@gmail.com>
2024-06-22 17:05:23 -0500

    basic commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,399 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import Dropzone from "react-dropzone";
+import { saveAs } from "file-saver";
+import { useState } from "react";
+import { FileRejection } from "react-dropzone";
+import { ThreeDots } from "react-loader-spinner";
+import { FaTrashAlt } from "react-icons/fa";
+import { FaDownload } from "react-icons/fa";
+import { XCircleIcon } from "@heroicons/react/20/solid";
+import { PhotoIcon } from "@heroicons/react/24/outline";
+import { SparklesIcon } from "@heroicons/react/24/outline";
+import { SelectMenu } from "@/app/selectmenu"
+import { ImageAreaProps } from "@/types";
+
+type ErrorNotificationProps = {
+  errorMessage: string;
+};
+
+type ActionPanelProps = {
+  isLoading: boolean;
+  submitImage(): void;
+};
+
+type UploadedImageProps = {
+  image: File;
+  removeImage(): void;
+  file: {
+    name: string;
+    size: string;
+  };
+};
+
+type ImageOutputProps = ImageAreaProps & {
+  loading: boolean;
+  outputImage: string | null;
+  downloadOutputImage(): void;
+};
+
+const themes = ["Modern", "Vintage", "Minimalist", "Professional"];
+const rooms = ["Living Room", "Dining Room", "Bedroom", "Bathroom", "Office"];
+
+const acceptedFileTypes = {
+  "image/jpeg": [".jpeg", ".jpg", ".png"],
+};
+
+const maxFileSize = 5 * 1024 * 1024; // 5MB
+
+/**
+ * Display an error notification
+ * @param {ErrorNotificationProps} props The component props
+ */
+function ErrorNotification({ errorMessage }: ErrorNotificationProps) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="mx-4 mb-10 rounded-md bg-red-50 p-4 lg:mx-6 xl:mx-8">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+        </div>
+        <div className="ml-3">
+          <p className="text-sm font-medium text-red-800">{errorMessage}</p>
         </div>
       </div>
+    </div>
+  );
+}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+/**
+ * Display the action panel
+ * @param {ActionPanelProps} props The component props
+ */
+function ActionPanel({ isLoading, submitImage }: ActionPanelProps) {
+  const isDisabled = isLoading;
+
+  return (
+    <section className="mx-4 bg-gray-900 shadow sm:rounded-lg lg:mx-6 xl:mx-8">
+      <div className="px-4 py-5 sm:p-6">
+        <div className="sm:flex sm:items-start sm:justify-between">
+          <div>
+            <h3 className="text-base font-semibold leading-6 text-gray-300 lg:text-xl">
+              Upload a photo or image
+            </h3>
+            <div className="mt-2 max-w-xl text-sm text-gray-500">
+              <p>
+                Upload an image of a room and let our AI generate a new design.
+              </p>
+            </div>
+          </div>
+          <div className="mt-5 sm:ml-6 sm:mt-0 sm:flex sm:flex-shrink-0 sm:items-center">
+            <button
+              type="button"
+              disabled={isDisabled}
+              onClick={submitImage}
+              className={`${
+                isDisabled
+                  ? "cursor-not-allowed bg-indigo-300 text-gray-300 hover:bg-indigo-300 hover:text-gray-300"
+                  : "bg-indigo-600 text-white"
+              } inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm transition-all duration-300 hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 lg:px-3.5 lg:py-2.5`}
+            >
+              Design this room
+              <SparklesIcon className="ml-2 h-4 w-4 text-gray-300" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+/**
+ * Display the image output
+ * @param {ImageOutputProps} props The component props
+ */
+function ImageOutput(props: ImageOutputProps) {
+  return (
+    <section className="relative min-h-[206px] w-full">
+      <button
+        type="button"
+        className={`${
+          props.loading ? "flex items-center justify-center" : ""
+        } relative block h-full w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+      >
+        {!props.outputImage && props.loading ? (
+          <span className="flex flex-col items-center">
+            <ThreeDots
+              height="50"
+              width="60"
+              color="#eee"
+              ariaLabel="three-dots-loading"
+              visible={props.loading}
+            />
+            <span className="block text-sm font-semibold text-gray-300">
+              Processing the output image
+            </span>
+          </span>
+        ) : null}
+
+        {!props.outputImage && !props.loading ? (
+          <>
+            <props.icon className="mx-auto h-12 w-12 text-gray-400" />
+            <span className="mt-2 block text-sm font-semibold text-gray-300">
+              {props.title}
+            </span>
+          </>
+        ) : null}
+
+        {!props.loading && props.outputImage ? (
+          <img
+            src={props.outputImage}
+            alt="output"
+            className="h-full w-full object-cover"
+          />
+        ) : null}
+      </button>
+
+      {!props.loading && props.outputImage ? (
+        <button
+          onClick={props.downloadOutputImage}
+          className="group absolute right-1 top-1 bg-yellow-500 p-2 text-black"
+        >
+          <FaDownload className="h-4 w-4 duration-300 group-hover:scale-110" />
+        </button>
+      ) : null}
+    </section>
+  );
+}
+
+/**
+ * Display the uploaded image
+ * @param {UploadedImageProps} props The component props
+ */
+function UploadedImage({ file, image, removeImage }: UploadedImageProps) {
+  return (
+    <section className="relative min-h-[206px] w-full">
+      <button className="relative block h-full w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        <img
+          src={URL.createObjectURL(image)}
+          alt={image.name}
+          className="h-full w-full object-cover"
         />
+      </button>
+
+      <button
+        className="group absolute right-1 top-1 rounded bg-yellow-500 p-2 text-black"
+        onClick={removeImage}
+      >
+        <FaTrashAlt className="h-4 w-4 duration-300 group-hover:scale-110" />
+      </button>
+
+      <div className="text-md absolute left-0 top-0 bg-opacity-50 p-2 pl-3.5 text-white">
+        {file.name} ({file.size})
       </div>
+    </section>
+  );
+}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+/**
+ * Display the image dropzone
+ * @param {ImageAreaProps} props The component props
+ */
+function ImageDropzone(
+  props: ImageAreaProps & {
+    onImageDrop(acceptedFiles: File[], rejectedFiles: FileRejection[]): void;
+  }
+) {
+  return (
+    <Dropzone
+      onDrop={props.onImageDrop}
+      accept={acceptedFileTypes}
+      maxSize={maxFileSize}
+      multiple={false}
+    >
+      {({ getRootProps, getInputProps }) => (
+        <>
+          <input {...getInputProps()} />
+          <button
+            {...getRootProps()}
+            type="button"
+            className="relative block min-h-[206px] w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            <props.icon className="mx-auto h-12 w-12 text-gray-400" />
+            <span className="mt-2 block text-sm font-semibold text-gray-300">
+              {props.title}
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          </button>
+        </>
+      )}
+    </Dropzone>
+  );
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+/**
+ * Display the home page
+ */
+export default function HomePage() {
+  const [outputImage, setOutputImage] = useState<string | null>(null);
+  const [base64Image, setBase64Image] = useState<string | null>(null);
+  const [theme, setTheme] = useState<string>(themes[0]);
+  const [room, setRoom] = useState<string>(rooms[0]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>("");
+  const [file, setFile] = useState<File | null>(null);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  /**
+   * Handle the image drop event
+   * @param {Array<File>} acceptedFiles The accepted files
+   * @param {Array<FileRejection>} rejectedFiles The rejected files
+   * @returns void
+   */
+  function onImageDrop(
+    acceptedFiles: File[],
+    rejectedFiles: FileRejection[]
+  ): void {
+    // Check if any of the uploaded files are not valid
+    if (rejectedFiles.length > 0) {
+      console.info(rejectedFiles);
+      setError("Please upload a PNG or JPEG image less than 5MB.");
+      return;
+    }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    removeImage();
+
+    console.info(acceptedFiles);
+    setError("");
+    setFile(acceptedFiles[0]);
+
+    // Convert to base64
+    convertImageToBase64(acceptedFiles[0]);
+  }
+
+  /**
+   * Convert the image to base64
+   * @param {File} file The file to convert
+   * @returns void
+   */
+  function convertImageToBase64(file: File): void {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const binaryStr = reader.result as string;
+      setBase64Image(binaryStr);
+    };
+  }
+
+  /**
+   * Convert the file size to a human-readable format
+   * @param {number} size The file size
+   * @returns {string}
+   */
+  function fileSize(size: number): string {
+    if (size === 0) {
+      return "0 Bytes";
+    }
+
+    const k = 1024;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    const i = Math.floor(Math.log(size) / Math.log(k));
+
+    return parseFloat((size / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+  }
+
+  /**
+   * Remove the uploaded image
+   * @returns void
+   */
+  function removeImage(): void {
+    setFile(null);
+    setOutputImage(null);
+  }
+
+  /**
+   * Download the output image
+   * @returns void
+   */
+  function downloadOutputImage(): void {
+    saveAs(outputImage as string, "output.png");
+  }
+
+  /**
+   * Submit the image to the server
+   * @returns {Promise<void>}
+   */
+  async function submitImage(): Promise<void> {
+    if (!file) {
+      setError("Please upload an image.");
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await fetch("/api/replicate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ image: base64Image, theme, room }),
+    });
+
+    const result = await response.json();
+    console.log(result);
+
+    if (result.error) {
+      setError(result.error);
+      setLoading(false);
+      return;
+    }
+
+    // Output returns an array of two images
+    // Here we show the second image
+    setOutputImage(result.output[1]);
+    setLoading(false);
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col py-10 lg:pl-72">
+      {error ? <ErrorNotification errorMessage={error} /> : null}
+      <ActionPanel isLoading={loading} submitImage={submitImage} />
+
+      {/* <section className="mx-4 mt-9 flex w-fit flex-col space-y-8 lg:mx-6 lg:flex-row lg:space-x-8 lg:space-y-0 xl:mx-8">
+        <SelectMenu
+          label="Model"
+          options={themes}
+          selected={theme}
+          onChange={setTheme}
+        />
+        <SelectMenu
+          label="Room type"
+          options={rooms}
+          selected={room}
+          onChange={setRoom}
+        />
+      </section> */}
+
+      <section className="mt-10 grid flex-1 gap-6 px-4 lg:px-6 xl:grid-cols-2 xl:gap-8 xl:px-8">
+        {!file ? (
+          <ImageDropzone
+            title={`Drag 'n drop your image here or click to upload`}
+            onImageDrop={onImageDrop}
+            icon={PhotoIcon}
+          />
+        ) : (
+          <UploadedImage
+            image={file}
+            removeImage={removeImage}
+            file={{ name: file.name, size: fileSize(file.size) }}
+          />
+        )}
+
+        <ImageOutput
+          title={`AI-generated output goes here`}
+          downloadOutputImage={downloadOutputImage}
+          outputImage={outputImage}
+          icon={SparklesIcon}
+          loading={loading}
+        />
+      </section>
     </main>
   );
 }
